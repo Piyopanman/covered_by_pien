9b0b66d108a41a8e83eefb9868a88e19e45f8482
Piyopanman <g19911kh@gm.tsuda.ac.jp>
2021-01-07 18:41:05 +0900

    入力された値が正しいかどうかの判定する部分の実装

--- src/HelloOpenCV.java
+++ src/HelloOpenCV.java
@@ -1,12 +1,14 @@
 import java.util.ArrayList;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 //
 // Detects faces in an image, draws boxes around them, and writes the results
@@ -27,41 +29,80 @@ class DetectFaceDemo {
 
     //x座標順に並び替え
     int size = faceDetections.toArray().length;
-    Rect[] test = new Rect[size];
+    Rect[] facesArray = new Rect[size];
     for(int i=0 ; i<size ; i++) {
-    	test[i] = faceDetections.toArray()[i];
+    	facesArray[i] = faceDetections.toArray()[i];
     }
+
 
     for(int i=0 ; i<size ; i++) {
     	int j = i;
-    	while(j>0 && test[j-1].x > test[j].x) {
-            Rect tmp = test[j];
-            test[j] = test[j-1];
-            test[j-1] = tmp;
+    	while(j>0 && facesArray[j-1].x > facesArray[j].x) {
+            Rect tmp = facesArray[j];
+            facesArray[j] = facesArray[j-1];
+            facesArray[j-1] = tmp;
             j--;
     	}
     }
 
 //    for(int i=0 ; i<size ; i++) {
-//    	System.out.println(test[i].x);
+//    	System.out.println(facesArray[i].x);
 //    }
 
+    //どれにぴえんをつけるか入力を待つ
     Scanner sc = new Scanner(System.in);
-    boolean flag = true;
+    boolean continueFlag = true;
     ArrayList<Integer> pienRegisterList = new ArrayList<Integer>();
 
-    while(flag) {
+    while(continueFlag) {
         System.out.print("左から何番目の人にぴえんをつけますか？: ");
-        int n;
-        try {
-            n = sc.nextInt();
-		} catch (InputMismatchException e) {
-			System.out.println("数値を入力してください");
-			continue;
-		}
-//        Integer N = new Integer(n);
-        pienRegisterList.add(n);
+        String n;
+        String yOrN;
 
+        n = sc.next();
+        //入力された値がintかどうかを判定
+        boolean intFlag = true;
+        for(int i=0 ; i<n.length() ; i++) {
+        	if(Character.isDigit(n.charAt(i))) {
+        		continue;
+        	}else {
+        		intFlag = false;
+        		break;
+        	}
+        }
+        if(intFlag) {
+        	int nn = Integer.parseInt(n);
+        	if(nn > size) {
+        		System.out.println("【エラー】数値 " + n + " は指定できません。0から" + size + "の間で指定してください。");
+        		continue;
+        	}
+        	pienRegisterList.add(nn);
+        	boolean yOrNFlag  =true;
+        	while(yOrNFlag) {
+        		System.out.print("他の人にもぴえんをつけますか？ [yes/no]: ");
+        		yOrN = sc.next();
+        		if(yOrN.equals("yes")) {
+        			yOrNFlag = false;
+        		}else if(yOrN.equals("no")) {
+        			yOrNFlag = false;
+        			continueFlag = false;
+        		}else {
+        			System.out.println("【エラー】yes か no で入力してください。");
+        		}
+        	}
+
+        }else {
+        	System.out.println("【エラー】int型の数値を入力してください");
+        }
+
+
+    }
+
+
+    sc.close();
+
+    for (Rect rect : faceDetections.toArray()) {
+        Imgproc.rectangle(image, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 255, 0));
     }
 
 
